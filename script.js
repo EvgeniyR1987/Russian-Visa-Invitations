15dca698f645333c41267a7f5d70101ba66a45db
EvgeniyR1987 <103533983+EvgeniyR1987@users.noreply.github.com>
2022-12-21 15:08:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
   form.addEventListener("submit", formSend);
   async function formSend(e) {
     e.preventDefault();
-    let error = formValidate(form);
     let formData = new FormData(form);
-    if (error===0) {
     let response = await fetch("sendmail.php", {
       method: "POST",
       body: formData
@@ -19,39 +17,5 @@ document.addEventListener("DOMContentLoaded", function () {
       } else {
         alert("Ошибка");
       }
-    } else {
-      alert("Заполните обязательные поля*");
     }
-  }
-  function formValidate(form) {
-    let error = 0;
-    let formReq = document.querySelectorAll("._req");
-    for (let index = 0; index < formReq.length; index++) {
-      const input = formReq[index];
-      formRemoveError(input);
-      if (input.classList.contains("_email")) {
-        if (emailTest(input)) {
-          formAddError(input);
-          error++;
-        }
-      } else {
-        if (input.value === "") {
-          formAddError(input);
-          error++;
-        }
-      }
-    }
-    return error;
-  }
-  function formAddError(input) {
-    input.parentElement.classList.add("_error");
-    input.classList.add("_error");
-  }
-  function formRemoveError(input) {
-    input.parentElement.classList.remove("_error");
-    input.classList.remove("_error");
-  }
-  function emailTest(input) {
-    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-  }
 });
